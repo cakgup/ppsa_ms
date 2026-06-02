5e9221608d57831e0e77953d7d694a0414a23ed7
Mukh. Gufron Ikhsan <mukh.gufron@ui.ac.id>
2026-06-02 14:12:25 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.PPSA_CONFIG = {
-  API_BASE_URL: "",
+  API_BASE_URL: "https://ppsa-management-v4-api.ittihadmasjidku.workers.dev",
   APP_NAME: "PPSA Management System",
-  DEMO_MODE: true
+  DEMO_MODE: false
 };
